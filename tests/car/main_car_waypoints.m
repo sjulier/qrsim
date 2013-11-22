ae820a83bd81bdb7f3db4fc6ae2ea4d195abad86
sjulier <S.Julier@cs.ucl.ac.uk>
2013-11-22 17:27:26 +0000

    Added copies of files to be used for a slightly more interesting task.

---
+++ tests/car/main_car_waypoints.m
@@ -0,0 +1,41 @@
+% first test with the car
+
+clear all
+close all
+
+% create simulator object
+qrsim = QRSim();
+
+% load task parameters and do housekeeping
+state = qrsim.init('TaskCarDriveInCircle');
+
+% number of steps we run the simulation for
+N = 3000;
+
+wp = state.platforms{1}.getX(1:3)
+
+% Null controller - keeps turning on a circle
+U{1}=[25; 1*pi/180];
+
+tstart = tic;
+
+for i=1:N,
+    tloop=tic;
+    % one should alway make sure that the uav is valid
+    % i.e. no collision or out of area event happened
+    if(state.platforms{1}.isValid())
+        % compute controls
+        % step simulator
+        qrsim.step(U);
+    end
+    % wait so to run in real time
+    wait = max(0,state.task.dt-toc(tloop));
+    pause(wait);
+end
+
+% get reward
+% qrsim.reward();
+
+elapsed = toc(tstart);
+
+fprintf('running %d times real time\n',(N*state.DT)/elapsed);
