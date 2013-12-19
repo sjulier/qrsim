7d4603a5dfce25c340bbef8074f9d3182ce2e368
sjulier <S.Julier@cs.ucl.ac.uk>
2013-12-19 16:46:53 +0000

    Changed the task to include two people. Simplified the main loop to call just what's required.

--- tests/person/TaskPersonWalkToWaypoints.m
+++ tests/person/TaskPersonWalkToWaypoints.m
@@ -34,21 +34,34 @@ classdef TaskPersonWalkToWaypoints<Task
             % originutmcoords is the location of the RVC (our usual flying site)
             % generally when this is changed gpsspacesegment.orbitfile and 
             % gpsspacesegment.svs need to be changed
-            [E N zone h] = llaToUtm([51.71190;-0.21052;0]);
+            [E,N,zone,h] = llaToUtm([51.71190;-0.21052;0]);
             taskparams.environment.area.originutmcoords.E = E;
             taskparams.environment.area.originutmcoords.N = N;
             taskparams.environment.area.originutmcoords.h = h;
             taskparams.environment.area.originutmcoords.zone = zone;
             taskparams.environment.area.graphics.type = 'AreaGraphics';
-            
+
+            % GPS
+            % The space segment of the gps system
+            taskparams.environment.gpsspacesegment.on = 0; % if off the gps returns the noiseless position
+            taskparams.environment.gpsspacesegment.dt = 0.2;
+
+            % Wind
+            % i.e. a steady homogeneous wind with a direction and magnitude
+            % this is common to all helicopters
+            taskparams.environment.wind.on = 0;
+
             %%%%% platforms %%%%%
             % Configuration and initial state for each of the platforms
             taskparams.entities(1).configfile = 'person_config';
+            taskparams.entities(2).configfile = 'person_config';
+
         end
         
         function reset(obj) 
             % initial state
-            obj.simState.entities{1}.setX([0;0;0;0;0;0]);
+            obj.simState.entities{1}.setX([0;0;0;0;0;0;-2;-2;0;0;0;0]);
+            obj.simState.entities{2}.setX([20;30;0;0;0;0;14;5;0;0;0;0]);
         end
         
         function r=reward(~) 
--- tests/person/main_person_waypoints.m
+++ tests/person/main_person_waypoints.m
@@ -1,4 +1,4 @@
-% first test with the car
+% first test with the person
 
 clear all
 close all
@@ -7,7 +7,7 @@ close all
 qrsim = QRSim();
 
 % load task parameters and do housekeeping
-state = qrsim.init('TaskCarDriveThroughWaypoints');
+state = qrsim.init('TaskPersonWalkToWaypoints');
 
 % number of steps we run the simulation for
 N = 3000;
@@ -15,22 +15,11 @@ N = 3000;
 % Set up a random set of waypoints
 wp = [0 200 200 0;0 0 200 200];
 
-% Waypoints controller
-controller = CarWaypointController(wp, state.DT);
-
 tstart = tic;
 
 for i=1:N,
     tloop=tic;
-    % one should alway make sure that the uav is valid
-    % i.e. no collision or out of area event happened
-    if(state.platforms{1}.isValid())
-        % compute controls
-        U{1} = controller.computeU(state.platforms{1}.getX());
-        %U = [0;0.02;0.595;0;12];
-        % step simulator
-        qrsim.step(U);
-    end
+    qrsim.step([]);
     % wait so to run in real time
     wait = max(0,state.task.dt-toc(tloop));
     pause(wait);
